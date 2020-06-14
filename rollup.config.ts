54c35d23204208d9c5f12b7dd58087f0a79e6093
nlffeng <nlffeng@163.com>
2020-06-14 22:09:20 +0800

    build: 调整rollup配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,35 +4,41 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
 const libraryName = 'white-react-state'
 
 export default {
-  input: `src/${libraryName}.ts`,
+  input: `src/index.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true, globals: { react: 'React' } },
+    { file: pkg.module, format: 'es', sourcemap: true, globals: { react: 'React' } },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: [
+    'react'
+  ],
   watch: {
     include: 'src/**',
   },
   plugins: [
     // Allow json resolution
     json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
 
     // Resolve source maps to the original source
     sourceMaps(),
+    terser({
+      include: [/^.+\.min\.js$/]
+    })
   ],
 }
