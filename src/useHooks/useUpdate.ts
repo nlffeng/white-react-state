2da70d402b44d66355d123a37418498d8e862ff3
nlffeng <nlffeng@163.com>
2020-06-21 11:29:52 +0800

    fix: 修复 useUpdate 更新机制

--- src/useHooks/useUpdate.ts
+++ src/useHooks/useUpdate.ts
@@ -1,8 +1,8 @@
 import { useCallback, useState } from 'react';
 
 const useUpdate = () => {
-  const [, setState] = useState(0);
-  return useCallback(() => setState((prevState: number) => (prevState === 0 ? 1 : 0)), []);
+  const [, setState] = useState({});
+  return useCallback(() => setState({}), []);
 };
 
 export default useUpdate;
